b3e2697b9cdb12563d62079797d209d82dbf5026
Masoud-z <masoud.zeinalabedini@gmail.com>
2025-02-27 11:18:58 +0330

    Set up basic route handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,57 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { JSX, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouteProps,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+type Routes = RouteProps & {
+  lazyElement?: React.LazyExoticComponent<() => JSX.Element>;
+};
+
+async function translationLoader(page: string) {
+  const translation = await new Promise<string[]>((resolve) =>
+    resolve([page, "Hello", "World"])
+  );
+  return { translation };
+}
+
+const queryClient = new QueryClient();
+const routes: Routes[] = [];
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<App />} loader={() => translationLoader("home")} />
+      {routes.map((route) => (
+        <Route
+          path={route.path}
+          loader={() => translationLoader("home")}
+          element={
+            route.lazyElement ? (
+              <Suspense fallback={<div>Loading...</div>}>
+                <route.lazyElement />
+              </Suspense>
+            ) : (
+              route.element
+            )
+          }
+        />
+      ))}
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
