46018e1977625c269ff22d8a37bffa3ea4f3d82e
Masoud-z <masoud.zeinalabedini@gmail.com>
2025-02-27 13:32:24 +0330

    Setup translation with i18n

--- src/main.tsx
+++ src/main.tsx
@@ -9,18 +9,12 @@ import {
   RouteProps,
   RouterProvider,
 } from "react-router-dom";
+import "./i18n/i18n";
 import App from "./App";
 
 type Routes = RouteProps & {
   lazyElement?: React.LazyExoticComponent<() => JSX.Element>;
 };
-
-async function translationLoader(page: string) {
-  const translation = await new Promise<string[]>((resolve) =>
-    resolve([page, "Hello", "World"])
-  );
-  return { translation };
-}
 
 const queryClient = new QueryClient();
 const routes: Routes[] = [];
@@ -28,11 +22,10 @@ const routes: Routes[] = [];
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      <Route index element={<App />} loader={() => translationLoader("home")} />
+      <Route index element={<App />} />
       {routes.map((route) => (
         <Route
           path={route.path}
-          loader={() => translationLoader("home")}
           element={
             route.lazyElement ? (
               <Suspense fallback={<div>Loading...</div>}>
@@ -51,7 +44,9 @@ const router = createBrowserRouter(
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
     </QueryClientProvider>
   </StrictMode>
 );
